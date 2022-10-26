de69d8111d458b7cb79eb52d77f792ec7bb40c76
Matthew Hamilton <Matthew.Hamilton@orygen.org.au>
2022-10-26 15:08:17 +1100

    more tweaks to yaml

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -59,7 +59,8 @@ z <- ready4::author(z)
 ready4::write_citation_cff(packageDescription("mychoice"),
                            citation_chr = readLines("inst/CITATION"))
 readLines(".github/workflows/R-CMD-check.yaml") %>%
-  stringr::str_replace_all("r-lib/actions/setup-r@master","r-lib/actions/setup-r@main") %>%
+  stringr::str_replace_all("r-lib/actions/setup-r@master","r-lib/actions/setup-r@v2") %>%
+  stringr::str_replace_all("r-lib/actions/setup-pandoc@master","r-lib/actions/setup-pandoc@v2") %>%
   writeLines(con = ".github/workflows/R-CMD-check.yaml")
 # usethis::use_dev_package("specific",
 #                          remote = "ready4-dev/specific")
